803d4f35fe7d1a38c3d2a4fab0d60dea9029cfb5
Miguel-MultiSafepay <104830395+Miguel-MultiSafepay@users.noreply.github.com>
2024-03-25 13:13:10 +0100

    DAVAMS-754: Fix "Template ID" within the Payment Component (#44)

--- catalog/controller/payment/multisafepay.php
+++ catalog/controller/payment/multisafepay.php
@@ -73,9 +73,12 @@ class Multisafepay extends Controller {
                 'locale' => $data['locale'],
                 'country' => $data['country'],
             ),
-            'template' => array(
-                'settings' => array(
-                    'embed_mode' => true
+            'payment_options' => array(
+                'template' => array(
+                    'settings' => array(
+                        'embed_mode' => true
+                    ),
+                    'merge' => true,
                 )
             )
         );
